05bbcdef2dfe7c4bb9d553a0f15ea0e01a0a7008
Adan De Santiago Avalos <adan1995a@gmail.com>
2017-04-13 23:33:53 -0500

    menu con css

--- src/nav.js
+++ src/nav.js
@@ -3,13 +3,15 @@ import { Link, IndexLink } from 'react-router';
 import {stack as Menu} from 'react-burger-menu';
 import Nosotros from './nosotros.js'
 import './App.css';
-
+var isMenuOpen= function(state){
+  return state.isOpen;
+};
 class SideBar extends React.Component {
 
     render() {
         return (
-          <Menu right width='100%'>
-          <a id="home" className="menu-item" href="#" >Home</a>
+          <Menu right width='100%' onStateChange={isMenuOpen}>
+           <a id="home" className="menu-item" href="/">Home</a>
           <a id="about" className="menu-item" href="#seccion-nosotros">Nosotros</a>
           <a id="contact" className="menu-item" href="#">Contact</a>
           <a onClick={ this.showSettings } className="menu-item--small" href="#">Settings</a>
