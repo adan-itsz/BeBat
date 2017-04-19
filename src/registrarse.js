b85ba6499bb6ba5344802e6d530a295d55e1cadf
Gabriela Reyes <Gabriela Reyes>
2017-04-19 17:32:53 -0500

    for registro listo

--- src/registrarse.js
+++ src/registrarse.js
@@ -11,8 +11,9 @@ class Registro extends React.Component {
           <img src="https://source.unsplash.com/QnjkbXywIyY/2100x1400"/>
           <h1 id="tituloRegistro"><Glyphicon glyph="chevron-right" />Registrate!</h1>
           <div id="regis">
-            <h3 id="h">INGRESA TUS DATOS</h3>
-            <form action="s.html" method="post">
+
+            <Form action="s.html" method="post">
+              <h3 id="h">INGRESA TUS DATOS</h3>
              <input type="text" placeholder="Nombre(s) *" required />
              <input type="text" placeholder="Apellido Paterno *" required/>
              <input type="text" placeholder="Apellido Materno *" required/>
@@ -20,20 +21,17 @@ class Registro extends React.Component {
              <input type="password" placeholder="Contraseña *" required/>
              <input type="text" placeholder="RFC " />
              <input type="text" placeholder="Telefono " />
+
              <a id="cue"href="#">¿Ya tienes una cuenta?</a>
 
-             <div id="res">
-              <input id = "check" type="checkbox" />
-              <label id="checkLabel">Acepto </label>
 
-
-               <a id="terminos"  href="#"> términos y condiciones</a>
-               </div>
+              <label id="checkLabel">Al presionar el botón aceptar, estarás de acuerdo con nuestros:<a id="terminos"  href="#"> términos y condiciones</a></label>
 
 
              <Button id="btnaceptar" type="submit" bsSize="large">Aceptar</Button>
-            </form>
+               </Form>
           </div>
+            <div id="espacio">espacio</div>
          </div>
         );
     }
