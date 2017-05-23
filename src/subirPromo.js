e1dc62488d8e751f5062469dc8489a0c643b750c
Adan De Santiago Avalos <adan1995a@gmail.com>
2017-05-23 18:47:03 -0500

    sube slides completos y crea carpeta

--- src/subirPromo.js
+++ src/subirPromo.js
@@ -18,6 +18,8 @@ class SubirPromo extends Component {
       uploadStatus:0,
       contador:0,
        arreglo:[],
+       arrayPreview:[],
+       nobreSlide:''
 
 
     }
@@ -26,10 +28,9 @@ class SubirPromo extends Component {
   agregarImagenes(a){
        if(a!=null && this.state.contador <= 5){
          this.setState({
-
-             picture: a,
-             contador: this.state.contador +=1,
-           arreglo:this.state.arreglo.concat([{url:this.state.imagePreviewUrl}])});
+          contador: this.state.contador +=1,
+          arrayPreview:this.state.arrayPreview.concat([{url:this.state.imagePreviewUrl}]),
+          arreglo:this.state.arreglo.concat([{a}])});
       }
     }
 
@@ -38,35 +39,39 @@ subeSlide(){
   var user = firebase.auth().currentUser;
   this.state.arreglo.map(listaAAgregar=>{
 
-    const file = event.target.files[0]
-    const storageRef = firebase.storage().ref(`${user.email}/${file.name}`)
+    const file = listaAAgregar.a
+    const storageRef = firebase.storage().ref(`${user.email}/${this.nombre_slide.value}/${file.name}`)
     const task = storageRef.put(file)
+    task.on('state_changed', snapshot => {
+             let per = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+             this.setState({
+                uploadStatus : per
+             })
+          }, (error) => {
+             this.setState({
+                message : `Alert!, ${error.message}`
+             })
+          }
+ 		 )
 
-         task.on('state_changed', snapshot => {
-            let per = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-            this.setState({
-               uploadStatus : per
-            })
-         }, (error) => {
-            this.setState({
-               message : `Alert!, ${error.message}`
-            })
-         })
    })
 }
 handleOnChange (event) {
   event.preventDefault();
   let reader = new FileReader();
  let file = event.target.files[0];
-
+alert(this.nombre_slide.value);
  reader.onloadend = () => {
    this.setState({
      file: file,
      imagePreviewUrl: reader.result
    });
+    this.agregarImagenes(file);
+
  }
 
  reader.readAsDataURL(file)
+
 
    }
   render() {
@@ -78,8 +83,16 @@ handleOnChange (event) {
      </progress>
 
        <input id="inputSubir" type='file' onChange={this.handleOnChange.bind(this)}/>
+        <button onClick={() => this.subeSlide()}>cargar</button>
+        <input id='nombre_slide' type='text' name="nombre_slide" ref={(nombre_slide) => this.nombre_slide = nombre_slide} required/>
+
+
        <div id='gallery'>
-      <img src={this.state.imagePreviewUrl}/>
+       <ul className="listaPromos">
+              {this.state.arrayPreview.map(listaImgs=>{
+                return (<Promo p={listaImgs.url}/>);})
+              }
+      </ul>
        </div>
      </div>
     );
