7c33b88a98ac93afc47320dd54b945b928d7633f
Adan De Santiago Avalos <adan1995a@gmail.com>
2017-06-01 11:54:55 -0500

    agrega a DB

--- src/subirPromo.js
+++ src/subirPromo.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
 import * as firebase from 'firebase'
 import './subirPromo.css';
+import { ref } from './constants.js'
+
+const database = firebase.database();
 
 class Promo extends Component{
   render(){
@@ -19,12 +22,20 @@ class SubirPromo extends Component {
       contador:0,
        arreglo:[],
        arrayPreview:[],
-       nobreSlide:''
+       arrayStorage:[]
 
 
     }
     this.agregarImagenes=this.agregarImagenes.bind(this);
+    this.subirDb=this.subirDB.bind(this);
   }
+
+  recortarCadenas(correo){
+    var remplazo=correo.split('.').join('-');
+    return remplazo;
+  }
+
+
   agregarImagenes(a){
        if(a!=null && this.state.contador <= 5){
          this.setState({
@@ -35,10 +46,26 @@ class SubirPromo extends Component {
     }
 
 
-subeSlide(){
-  var user = firebase.auth().currentUser;
-  this.state.arreglo.map(listaAAgregar=>{
+    subirDB(){
+      var user = firebase.auth().currentUser;
+      var correoUsuario=this.recortarCadenas(`${user.email}`);
+      alert(correoUsuario);
+      var refDBHistorial=ref.child(correoUsuario+"/Historial");
+     var refDB=ref.child(correoUsuario+"/SlideActual");
+      var HistorialSlidesDB=refDBHistorial.push();
+      HistorialSlidesDB.set({
+        historial:`${this.state.arreglo}`
+      });
+   refDB.set({
+        slideActual:`${this.state.arreglo}`,
+        contador:3
+      });
+    }
 
+
+subeSlide(){
+  this.state.arreglo.map(listaAAgregar=>{
+    var user = firebase.auth().currentUser;
     const file = listaAAgregar.a
     const storageRef = firebase.storage().ref(`${user.email}/${this.nombre_slide.value}/${file.name}`)
     const task = storageRef.put(file)
@@ -51,29 +78,33 @@ subeSlide(){
              this.setState({
                 message : `Alert!, ${error.message}`
              })
-          }
+          },
+          () => {
+            this.setState({
+               picture : task.snapshot.downloadURL,
+              // arrayStorage:this.state.arrayStorage.concat([{url:this.state.picture}])
+            })}
  		 )
+   });
+   this.subirDB();
 
-   })
 }
+
+
 handleOnChange (event) {
   event.preventDefault();
   let reader = new FileReader();
  let file = event.target.files[0];
-alert(this.nombre_slide.value);
  reader.onloadend = () => {
    this.setState({
      file: file,
      imagePreviewUrl: reader.result
    });
     this.agregarImagenes(file);
-
  }
-
  reader.readAsDataURL(file)
-
-
    }
+
   render() {
 
     return (
