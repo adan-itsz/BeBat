907321930a13774e67697a5b9759ac7c5d04aa29
Gabriela Reyes <Gabriela Reyes>
2017-06-12 22:48:49 -0600

    diseno nosotros temporal

--- src/nosotros.js
+++ src/nosotros.js
@@ -1,18 +1,28 @@
 import React, { Component,PropTypes } from 'react';
 import './nosotros.css';
+import celular from './celphoneBat.png'
 
 class Nosotros extends React.Component {
     render() {
         return (
           <div id="nos">
           <h1>Nosotros</h1>
-          <p>Lorem ipsum dolor sit amet, consectetuer molestie dolor sit rutrum, arcu condimentum consectetuer pellentesque ante curabitur.
-            Aliquet sed sed enim donec, wisi ut libero lorem, sit incidunt, enim proin justo at natoque, convallis odio lorem varius fermentum egestas est.
-            Imperdiet et quam porta maecenas in integer, tellus wisi, vel sapien mauris, varius recusandae diam nec leo odio, morbi quis purus bibendum faucibus id.
-            Nec ligula fusce natoque lorem.
-           </p>
-           <img src='https://proximi.io/wp-content/uploads/2016/02/kuva_etusivulle.png'/>
-
+          <div id='nosotros-content'>
+             <img id='nosotros-celular' src={celular}/>
+             <div id='nosotros-parrafos'>
+             <p className='nosotros-parrafo'> Somos una empresa dedicada
+                 a cambiar la manera en la que
+                 los consumidores <span className='nosotros-color'>interactuan </span>
+                 con las marcas, abriendo paso
+                 a la era del <span className='nosotros-color'>Physical Web</span>.
+              </p>
+              <p className='nosotros-parrafo'>
+                Creemos que la tecnología beacon tiene
+                el <span className='nosotros-color'>potencial</span> de hacer mucho, mucho más, para
+                 los consumidores, las marcas y los <span className='nosotros-color'>vendedores</span>.
+               </p>
+              </div>
+           </div>
           </div>
 
         );
