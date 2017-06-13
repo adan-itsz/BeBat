e176187a91e9bf369330b974788cebe699206cd6
Gabriela Reyes <Gabriela Reyes>
2017-06-13 07:47:13 -0600

    diseno beneficios temporal

--- src/servicios.js
+++ src/servicios.js
@@ -4,108 +4,25 @@ import './servicios.css'
 import Broadcast from 'react-icons/lib/go/broadcast'
 import Rigth from 'react-icons/lib/ti/chevron-right-outline'
 import Left from 'react-icons/lib/ti/chevron-left-outline'
+import beacon from './beacon1.png'
+import beacon2 from './beacon2.png'
 
-
-
-class PrimeraImagen extends React.Component{
-  constructor(props){
-    super(props);
-    this.state ={
-      showComponent:false,
-      name:'tileOne'
-    }
-    this.onClickLink = this.onClickLink.bind(this);
-  }
-  onClickLink(){
-    this.setState({
-      showComponent:!this.state.showComponent,
-      name:'tileBack'
-    });
-  }
-
-  render(){
-    let isClicked = this.state.showComponent;
-    let tile = null;
-    if(isClicked){
-      tile = <OtroLado />
-    }
-    else{
-      tile =
-          <div className="textWrapper">
-            <a onClick={this.onClickLink} ><div className="btnservice">Ipsum</div></a>
-          </div>
-    }
-    return(
-      <div className="tile" id={this.state.name}>
-        {tile}
-      </div>
-    );
-  }
-}
-
-class SegundaImagen extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      showComponent: false,
-      name: 'tileTwo'
-    }
-    this.onClickLink = this.onClickLink.bind(this);
-  }
-  onClickLink(){
-    this.setState({
-      showComponent:!this.state.showComponent,
-      name: 'tileBack'
-    });
-  }
-  render(){
-    let isClicked = this.state.showComponent;
-    let tile = null;
-    if(isClicked){
-      tile = <OtroLado />
-    }else {
-      tile =
-        <div className="textWrapper">
-          <a onClick={this.onClickLink}><div className="btnservice">Ipsum</div></a>
-        </div>
-
-    }
-    return(
-      <div className ="tile" id={this.state.name}>
-        {tile}
-      </div>
-    );
-  }
-}
-
-class OtroLado extends React.Component{
-  render(){
-    return(
-        <div className="textWrapper">
-        <div id ="puntos">
-          <Broadcast id="broadcast-icon" />
-          <p><Rigth />Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-           <p><Rigth />Aenean commodo ligula eget dolor.</p>
-           <p><Rigth />Aenean massa.</p>
-           <p><Rigth />Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-        </div>
-        </div>
-    )
-  }
-}
 
 class Servicios extends React.Component {
-    constructor(props){
-      super(props);
-    }
     render() {
-      let tileOne = <PrimeraImagen />
-      let tileTwo = <SegundaImagen />
         return (
         		<div id="servi">
-              <div className="imageGrid">
-                {tileOne}
-                {tileTwo}
+              <div id="servicios-imagen">
+                <h3 id='servicios-titulo'>Beneficios</h3>
+                <div id='beacon-yw'>
+                  <img id='beacon-amarillo'src={beacon}/>
+                </div>
+                <ul id='servicios-lista'>
+                  <li>-Información relevante en tiempo real</li>
+                  <li>-Mayor Alcance</li>
+                  <li>-Experiencia envolvente</li>
+                  <li>-Más Clientes</li>
+                </ul>
               </div>
         		</div>
         );
